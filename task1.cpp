cf356cdecf1353090c01410c3430f6c789f7e7cb
DLman12 <91415803+DLman12@users.noreply.github.com>
2021-10-03 22:18:41 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	const double arg = 0.123;
+	double answer = (cos(arg) + sin(arg)) / (cos(arg) - sin(arg));
+	cout << setprecision(5) << "x = " << arg << endl;
+	cout << setprecision(5) << "f = " << answer << endl;
+
+	double sec_arg = 0;
+	cout << "Enter a value: ";
+	cin >> sec_arg;
+	answer = (cos(sec_arg) + sin(sec_arg)) / (cos(sec_arg) - sin(sec_arg));
+	cout << setprecision(5) << "x = " << sec_arg << endl;
+	cout << setprecision(5) << "f = " << answer << endl;
+}
